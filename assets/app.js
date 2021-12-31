279deb68d02a3edae30206e339f855f450a8dc12
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2021-12-30 22:26:09 -0500

    city buttons work for current day weather, search button not work on click or enter keypress, and I need api for forecast

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,23 @@
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
-let name = document.querySelector('.name')
+let cityname = document.querySelector('.name')
 var desc = document.querySelector('.desc')
 var temp = document.querySelector('.temp')
 
 button.addEventListener('click', function() {
-    fetch('//api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8e04b40f20b6603b082aa92e3bc63978')
+    fetch('//api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
 
-    .then(response => response.json())
+    .then(response => response.json()) 
     .then(data => {
         let nameValue = data['name'];
         var tempValue = data['main']['temp']
         var descValue = data['weather'][0]['description']
 
 
-        name.innerHTML = nameValue;
+        cityname.innerHTML = nameValue;
         temp.innerHTML = tempValue;
         desc.innerHTML = descValue;
-        console.log(data['name'])
+        console.log(data)
     })
 
     .catch(err => alert("wrong city name!"))
--- assets/logic.js
+++ assets/logic.js
@@ -1,18 +1,272 @@
 
-class Fetch {
-    async getCurrent (input) {
-        var APIKey = '8e04b40f20b6603b082aa92e3bc63978';
-        city = "chicago";
+var Aus_Button = document.querySelector('.austin')
+var Chi_Button = document.querySelector('.chicago')
+var NY_Button = document.querySelector('.newyork')
+var ORL_Button = document.querySelector('.orlando')
+var SF_Button = document.querySelector('.sanfrancisco')
+var Sea_Button = document.querySelector('.seattle')
+var Den_Button = document.querySelector('.denver')
+var ATL_Button = document.querySelector('.atlanta')
 
-    }
+
+
+var inputValue = document.querySelector('.inputValue')
+let cityname = document.querySelector('#city-date')
+var temp = document.querySelector('#main-temp')
+var wind = document.querySelector('#main-wind')
+var humidity = document.querySelector('#main-humidity')
+var uv_index = document.querySelector('#uv-index')
+
+function getWeather() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => console.log(data))
+    
+
+    .catch(err => alert('Wrong city name!'))
 }
 
-// var APIKey = '8e04b40f20b6603b082aa92e3bc63978';
-// var city;
+
+ATL_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=atlanta&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
 
 
-`http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${APIKey} `
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
 
-const data = await response.json() ;
 
-console.log(data)
+
+    
+
+
+})
+
+Den_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=denver&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+Sea_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=seattle&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+
+
+SF_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=san francisco&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+ORL_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=orlando&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+NY_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=new york&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+Chi_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=chicago&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+Aus_Button.addEventListener('click', function() {
+    fetch('//api.openweathermap.org/data/2.5/weather?q=austin&appid=8e04b40f20b6603b082aa92e3bc63978&units=imperial')
+
+    .then(response => response.json()) 
+    .then(data => {
+
+        let nameValue = data['name'];
+        var tempValue = data['main']['temp']
+        var humidityValue = data['main']['humidity']
+        var windValue = data['wind']['speed']
+        var descValue = data['weather'][0]['description']
+
+
+        cityname.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        humidity.innerHTML = humidityValue;
+        wind.innerHTML = windValue;
+        uv_index.innerHTML = descValue;
+        console.log(data)
+    })
+
+
+
+    
+
+
+})
+
+
+
+
+// var search = document.querySelector('form');
+// ["click", "keypress"].forEach(ev=>{
+//   search.addEventListener(ev, function(e){
+//      if(ev=="click"){
+//        console.log("click");//clicked
+//        getWeather();
+//      }
+//      if(e.keyCode==13){
+//       console.log("click");//enter key pressed
+//       getWeather();
+//      }
+//   });
+// });
+
